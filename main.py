88973225fe2ff0870b01d35c708b81affac5d993
andrew_linux_laptop <andrewducdanguniv@gmail.com>
2024-01-23 09:05:08 -0800

    key xor, no padding yet

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-
 from Crypto.Cipher import AES
-from Crypto.Hash import HMAC, SHA256
 from Crypto.Random import get_random_bytes
+from Crypto.Util.strxor import strxor
 
-data = 'secret data to transmit'.encode()
-
-
-
-
-def get_aes_key():
-    get_random_bytes(16)
 
 def main():
+    data = 'secret data to transmit'.encode()
+
+    #padding not done, len 16 should be different
+    len16 = len(data) // 16
+
+    aes_key = get_random_bytes(16)
+    ciphertext = b''
+
+    for i in range (len16):
+        ciphertext += strxor(aes_key, data[i : i+16])
+    
+    print(ciphertext)
     
 
-
 if __name__ == '__main__':
-    aes_key =  get_aes_key()
+    main()
