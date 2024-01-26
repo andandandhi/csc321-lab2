63ad49ff941de656c053eefd58d723049bb812e3
Sharon Pagidipati <sp14517@gmail.com>
2024-01-25 18:23:34 -0800

    update on ECB

--- aes.py
+++
@@ -1,8 +0,0 @@
-import Crypto
-
-
-def aes():
-    x = 0
-
-if __name__ == '__main__':
-    aes()
--- main.py
+++ main.py
@@ -5,15 +5,33 @@ from Crypto.Random import get_random_bytes
 
 data = 'secret data to transmit'.encode()
 
+words = "asdfghjklqwertyu"
+
+#can use pycryptodome's ciper to do decryption
 
 
 
 def get_aes_key():
     get_random_bytes(16)
-
+#block size is exactly 16 and block size is less than 16
+    #can have fixed string and test that
 def main():
+    print("hello")
+    
+    aes_key =  get_random_bytes(16)
+    cipher = AES.new(aes_key, AES.MODE_ECB)
+
+    #how do we XOR the aes key with words (our plaintext)
+    print(cipher) #returning object location and not the legible cipher
+
+
+
+ 
     
 
-
+#in order to XOR character into byte, convert character into ascii (ord or char)
+   
+   
 if __name__ == '__main__':
-    aes_key =  get_aes_key()
+    main()
+    
