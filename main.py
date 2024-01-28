7cb4abb682c47852fb31986e42a7497cba00b512
andrew_linux_laptop <andrewducdanguniv@gmail.com>
2024-01-27 17:53:02 -0800

    reads and pads file

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-from Crypto.Util.strxor import strxor
+import sys
+
+def file_reader():
+    n = len(sys.argv)
+    if(n < 1):
+        print("Missing file name")
+    
+    filename = sys.argv[1]
+    with open(filename, "rb") as file:
+        buf = file.read(16)
+        while len(buf) == 16:
+            a = ecb_encrypt(buf)
+            buf = file.read(16)        
+        a = ecb_encrypt(pkcs7(buf))
+
+def pkcs7(buf: bytes) -> bytes:
+    pad_len =  16 - len(buf)
+    return pad_len.to_bytes(2, 'big') * pad_len
 
 
 def get_aes_key():
     get_random_bytes(16)
 
-def main():
+def ecb_encrypt(plaintextBlock: bytes, aes_key: bytes) -> bytes:
     KEY_SIZE = 16   
     plaintextBlock = "16_byte_in_block"
     aes_key = get_random_bytes(16)
