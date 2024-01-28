6091e09aa7bccac578106c25cc2250eeb2235cb4
andrew_linux_laptop <andrewducdanguniv@gmail.com>
2024-01-27 18:10:27 -0800

    input output of ecb functions works

--- main.py
+++ main.py
@@ -2,53 +2,39 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import sys
 
+KEY_SIZE = 16
+
 def file_reader():
+    aes_key = get_random_bytes(16)
+    
     n = len(sys.argv)
     if(n < 1):
         print("Missing file name")
     
     filename = sys.argv[1]
-    with open(filename, "rb") as file:
-        buf = file.read(16)
-        while len(buf) == 16:
-            a = ecb_encrypt(buf)
-            buf = file.read(16)        
-        a = ecb_encrypt(pkcs7(buf))
+    with open(filename, "rb") as in_file, \
+         open("ecb_ciphertext", "wb") as ecb_file:
+        buf = in_file.read(KEY_SIZE)
+        while len(buf) == KEY_SIZE:
+            ecb_file.write(ecb_encrypt(buf, aes_key))
+            buf = in_file.read(KEY_SIZE)        
+        ecb_file.write(ecb_encrypt(pkcs7(buf)))
 
 def pkcs7(buf: bytes) -> bytes:
-    pad_len =  16 - len(buf)
+    pad_len =  KEY_SIZE - len(buf)
     return pad_len.to_bytes(2, 'big') * pad_len
 
-
-def get_aes_key():
-    get_random_bytes(16)
-
 def ecb_encrypt(plaintextBlock: bytes, aes_key: bytes) -> bytes:
-    KEY_SIZE = 16   
-    plaintextBlock = "16_byte_in_block"
-    aes_key = get_random_bytes(16)
     simpleCipher = AES.new(aes_key, AES.MODE_ECB)
     
-    
-    encryptedBlock = simpleCipher.encrypt(bytes(plaintextBlock, 'utf-8')) # must cast string to bytes for encryption
-    
+    encryptedBlock = simpleCipher.encrypt(plaintextBlock)
     print("original block:", plaintextBlock)
     print("encrypted block:", encryptedBlock)
     print("\nperforming decryption:")
-    
     decryptedBlock = simpleCipher.decrypt(encryptedBlock)
-    print(decryptedBlock.decode('utf-8'))
-    #plaintexttwo = str(decryptedBlock)
-# What is decryptedBlock? String or bytes?
-    #print(plaintexttwo)
+    print(decryptedBlock)
 
-    
-
-    #padding not done, len 16 should be different
-    
-   
-    
 
 if __name__ == '__main__':
-    main()
+    file_reader()
 
