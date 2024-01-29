0677d98a1275d7b50e56d94d78e311c64700e5b8
andrew_linux_laptop <andrewducdanguniv@gmail.com>
2024-01-29 13:11:20 -0800

    remove some comments

--- main.py
+++ main.py
@@ -62,9 +62,7 @@ def xor(words, iv):
 
 
 def cbc():
-    KEY_SIZE = 16  
-   
-    #plaintextBlock = "16_byte_in_block"
+    KEY_SIZE = 16 
     
     plaintextBlock = get_random_bytes(16)
     init_v = get_random_bytes(16)
@@ -74,7 +72,6 @@ def cbc():
     print(xor_val)
     
     simpleCipher = AES.new(key, AES.MODE_ECB)
-    #encryptedBlock = simpleCipher.encrypt(bytes(plaintextBlock, 'utf-8'))
     print(simpleCipher)
     encryptedBlock = simpleCipher.encrypt(xor_val)# must cast string to bytes for encryption
     
@@ -82,7 +79,6 @@ def cbc():
     #word = conversion(plaintextBlock)
 
     decryptedBlock = simpleCipher.decrypt(encryptedBlock)
-    # What is decryptedBlock? String or bytes?
     print(decryptedBlock)
 
    
