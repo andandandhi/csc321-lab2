94e6e666eabbf0ae30aa09a98c1f0399bc3872a2
Sharon Pagidipati <sp14517@gmail.com>
2024-01-29 11:11:27 -0800

    pushing new changes in cbc

--- main.py
+++ main.py
@@ -39,15 +39,19 @@ def conversion(word):
     return conversion
 
 #words, iv)
-def xor():
-    words = get_random_bytes(16)
-    iv = get_random_bytes(16)
-    code = ""
-    for i in range(0,len(words),2):
-        for j in range(0,len(iv),2):
-            xor_val = i ^ j
-            code = code + str(xor_val)
+def xor(words, iv):
+   # words = get_random_bytes(16)
+   # iv = get_random_bytes(16)
+    print(iv)
+    code = b""
+    for i in range(0,len(words)):
+        byte_1 = words[i]
+        byte_2 = iv[i]
+        xor_val = bytes([byte_1 ^ byte_2])
+        code = code + xor_val
     print(code)
+    print(len(iv))
+    print(len(code))
     return code
 
 
@@ -58,22 +62,19 @@ def cbc():
     init_v = "4F8D3E72A91B5C6EF20A9D4B1E73FBCD"
     #plaintextBlock = "16_byte_in_block"
     
-    #plaintextBlock = get_random_bytes(16)
-    #init_v = get_random_bytes(16)
-    hex_1 = plaintextBlock.hex()
-    hex_2 = init_v.hex()
-    #print(hex_1)
-    xor_val_literal = plaintextBlock ^ init_v
-    #xor_val = bytes.fromhex(xor_val_literal[2:])
+    plaintextBlock = get_random_bytes(16)
+    init_v = get_random_bytes(16)
+    xor_val = xor(plaintextBlock, init_v)
     print("xor val:")
-    print(xor_val_literal)
+    print(xor_val)
     
-    #simpleCipher = AES.new(xor_val, AES.MODE_ECB)
-    #print(simpleCipher)
+    simpleCipher = AES.new(xor_val, AES.MODE_ECB)
+    #encryptedBlock = simpleCipher.encrypt(bytes(plaintextBlock, 'utf-8'))
+    print(simpleCipher)
     #encryptedBlock = simpleCipher.encrypt(bytes(str(xor_val), 'utf-8')) # must cast string to bytes for encryption
     
    
-    #print("encrypted block:", encryptedBlock)
+   # print("encrypted block:", encryptedBlock)
     #word = conversion(plaintextBlock)
    
     #print(word)
@@ -81,5 +82,6 @@ def cbc():
     
 
 if __name__ == '__main__':
-    xor()
+    #xor()
+    cbc()
 
