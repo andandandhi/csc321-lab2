7157ebdaf3254750eb868c5703510eb9adf65e1f
andrew_linux_laptop <andrewducdanguniv@gmail.com>
2024-01-30 20:51:59 -0800

    test 16pad and 15pad string split

---
+++ tests.py
@@ -0,0 +1,29 @@
+import unittest
+from tasktwo import get_buf
+
+class TestTaskTwo(unittest.TestCase):
+    def test_16pad_submit(self):
+        bytes_in = ("userid=456;userdata=" + \
+                    "12345678901" + \
+                    ";session-id=31337").encode('utf-8')
+        expected = [bytes("userid=456;userd", 'utf-8'),
+                    bytes("ata=12345678901;", 'utf-8'),
+                    bytes("session-id=31337", 'utf-8'),
+                    bytes([16] * 16)]
+        result = list(get_buf(bytes_in))
+        assert(expected == result)
+            
+    def test_15pad_submit(self):
+        bytes_in = ("userid=456;userdata=" + \
+                    "123456789012" + \
+                    ";session-id=31337").encode('utf-8')
+        expected = [bytes("userid=456;userd", 'utf-8'),
+                    bytes("ata=123456789012", 'utf-8'),
+                    bytes(";session-id=3133", 'utf-8'),
+                    bytes([ord('7')] + ([15] * 15))]
+        result = list(get_buf(bytes_in))
+        assert(expected == result)
+
+    
+if __name__ == '__main__':
+    unittest.main()
