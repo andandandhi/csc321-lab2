310bec20bc31081279a92048073a886bbc53f234
andrew_linux_laptop <andrewducdanguniv@gmail.com>
2024-01-30 19:56:19 -0800

    encrypts with no runtime errors

--- tasktwo.py
+++ tasktwo.py
@@ -6,12 +6,12 @@ from typing import Generator
 
 KEY_SIZE = 16
 #below is for task 2
-aes_key = get_random_bytes(KEY_SIZE)
-iv = get_random_bytes(KEY_SIZE)
+AES_KEY = get_random_bytes(KEY_SIZE)
+IV = get_random_bytes(KEY_SIZE)
 
 def get_buf(message: bytes) -> Generator[bytes, None, None]:
     buf = bytearray()
-    for b in bytes:
+    for b in message:
         if len(buf) >= 16:
             yield bytes(buf)
             buf = bytearray()
@@ -19,7 +19,7 @@ def get_buf(message: bytes) -> Generator[bytes, None, None]:
             buf.append(b)
     yield pkcs7(bytes(buf))
 
-def submit():
+def submit() -> bytes():
     words = input("Type words here: ")
     new_words = ""
     for i in words:
@@ -35,11 +35,13 @@ def submit():
     data = data + ";session-id=31337"
     byte_data = data.encode('utf-8')
     
+    ciphertext = bytearray()
+    prev_cipherblock = IV
     for byte16 in get_buf(byte_data):
-        _
+        cipherblock = cbc_encrypt(byte16, AES_KEY, prev_cipherblock)
+        ciphertext.extend(cipherblock)
+        prev_cipherblock = cipherblock
+    return(bytes(ciphertext))
 
-    final_enc = cbc_encrypt(new_data, aes_key, iv)
 
-    print(final_enc)
-
-submit()
+print(submit())
