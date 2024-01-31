f4d4ea63fdead16620a3bf37f5be3f5df2913231
andrew_linux_laptop <andrewducdanguniv@gmail.com>
2024-01-31 12:38:39 -0800

    call hack in Tests

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from tasktwo import get_buf_pad
+from tasktwo import get_buf_pad, submit, cbc_decrypt, hack
 
 class TestTaskTwo(unittest.TestCase):
     def test_16pad_submit(self):
@@ -24,5 +24,16 @@ class TestTaskTwo(unittest.TestCase):
         result = list(get_buf_pad(bytes_in))
         assert(expected == result)
     
+    def test_hack(self):
+        assert(hack())
+
+    def not_a_test(self):
+        """This is only here to run the program with user input, don't run this test.
+        Call this function if you want to see the your decrypted submit() message.
+        """
+        encrypted_message = submit()
+        print(cbc_decrypt(encrypted_message))
+        assert(True)
+
 if __name__ == '__main__':
     unittest.main()
