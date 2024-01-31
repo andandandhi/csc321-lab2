2a12688913814246c343252a74e8df6741142e42
andrew_linux_laptop <andrewducdanguniv@gmail.com>
2024-01-30 19:44:25 -0800

    built generator for bytes buffer

--- tasktwo.py
+++ tasktwo.py
@@ -1,12 +1,23 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-import sys
 from main import cbc_encrypt, pkcs7
+from typing import Generator
+
 
 KEY_SIZE = 16
 #below is for task 2
 aes_key = get_random_bytes(KEY_SIZE)
 iv = get_random_bytes(KEY_SIZE)
+
+def get_buf(message: bytes) -> Generator[bytes, None, None]:
+    buf = bytearray()
+    for b in bytes:
+        if len(buf) >= 16:
+            yield bytes(buf)
+            buf = bytearray()
+        else:
+            buf.append(b)
+    yield pkcs7(bytes(buf))
 
 def submit():
     words = input("Type words here: ")
@@ -23,7 +34,10 @@ def submit():
     data = data + new_words #to add user words
     data = data + ";session-id=31337"
     byte_data = data.encode('utf-8')
-    new_data = pkcs7(byte_data)
+    
+    for byte16 in get_buf(byte_data):
+        _
+
     final_enc = cbc_encrypt(new_data, aes_key, iv)
 
     print(final_enc)
