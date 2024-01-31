440cda1617350307fb1b8ddb99e869d8b37334bb
andrew_linux_laptop <andrewducdanguniv@gmail.com>
2024-01-30 22:42:59 -0800

    accidentally finished

--- tasktwo.py
+++ tasktwo.py
@@ -1,6 +1,6 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-from main import cbc_encrypt, pkcs7
+from main import cbc_encrypt, pkcs7, xor
 from typing import Generator
 
 KEY_SIZE = 16
@@ -33,6 +33,8 @@ def get_buf_pad(message: bytes) -> Generator[bytes, None, None]:
 def get_buf(message: bytes) -> Generator[bytes, None, None]:
     """Splits the message into 16 byte blocks.
     Len(input) must be multiple of 16
+    Does NOT remove padding. 
+    TODO: remove padding.
     """
     buf = bytearray()
     for b in message:
@@ -65,19 +67,66 @@ def submit() -> bytes:
         prev_cipherblock = cipherblock
     return(bytes(ciphertext))
 
+def submit_manual_input(words: str) -> bytes:
+    """Same as submit(), but with args instead of input.
+    Used to work on hack(), delete this function when finished.
+    """
+    new_words = ""
+    for i in words:
+        if i == "=":
+            new_words = new_words + "%3D"
+        elif i == ";":
+            new_words = new_words + "%3B"
+        else:
+            new_words = new_words + i
+    data = ""
+    data = data + "userid=456;userdata="
+    data = data + new_words
+    data = data + ";session-id=31337"
+    byte_data = data.encode('utf-8')
+    
+    ciphertext = bytearray()
+    prev_cipherblock = IV
+    for byte16 in get_buf_pad(byte_data):
+        cipherblock = cbc_encrypt(byte16, AES_KEY, prev_cipherblock)
+        ciphertext.extend(cipherblock)
+        prev_cipherblock = cipherblock
+    return(bytes(ciphertext))
 
-def verify(ciphertext: bytes):
-    algorithm = AES.new(AES_KEY, AES.MODE_ECB)
-    
-    for cipherblock in get_buf:
-        prev_cipherblock = cipherblock #scope == verify(), not for-loop
-        
 
-    xor_val = xor(plaintextBlock, init_v) #xoring plaintext and init vector
-    
-    
-    encryptedBlock = simpleCipher.encrypt(xor_val)
-    
+def cbc_decrypt(ciphertext: bytes) -> str:
+    """Decrypts CBC ciphertext.
+    Does NOT remove padding. 
+    """
+    algorithm = AES.new(AES_KEY, AES.MODE_ECB)    
+    decrypted_bytearray = bytearray()
+    prev_cipherblock = IV
+    for cipherblock in get_buf(ciphertext):
+        aes_out = algorithm.decrypt(cipherblock)
+        plainblock = xor(aes_out, prev_cipherblock)
+        decrypted_bytearray.extend(plainblock)
+        prev_cipherblock = cipherblock
+    return decrypted_bytearray.decode('utf-8')
+
+def verify(ciphertext: bytes) -> bool:
+    plaintext = cbc_decrypt(ciphertext)
+    return bool(plaintext.find(';admin=true'))
+
+def hack():
+    encrypted_message=submit_manual_input('9admin9true')
+    em_ba = bytearray(encrypted_message)
+    em_ba[4] = em_ba[4] ^ (1 << 1) #index on bytearray returns int
+    em_ba[10] = em_ba[10] ^ (1 << 2)
+    # userid=456;userd
+    # ata=9admin9true5
+    # '9' + 0x02 = ';'   '9' + 0x04 = '='
+    # add 2 to 5th byte, add 4 to 11th byte
+    malcious_msg = encrypted_message
+    debug_print(malcious_msg, True)
+    print(verify(malcious_msg))
 
 if __name__ == '__main__':
-    debug_print(submit(), True)
+    #encrypted_message = submit()
+    #debug_print(encrypted_message)
+    #debug_print("\n"+cbc_decrypt(encrypted_message), True)
+    hack()
